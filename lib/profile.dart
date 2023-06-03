45009da384a21bc3a6989abc1b28c37fe39ac52b
onana <onanalaeticia6@gmail.com>
2023-06-03 14:51:42 +0100

    le fichier profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,10 @@
-import 'dart:html';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'dart:io';
 
 
 class profile extends StatefulWidget {
@@ -10,6 +15,143 @@ class profile extends StatefulWidget {
 }
 
 class _profileState extends State<profile> {
+
+  late String username ;
+  bool showControls = false;
+  List<VideoPlayerController> controllers = [];
+  List<String> videoLinks = [
+    // 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+    // 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+  ];
+  final user = FirebaseAuth.instance.currentUser!;
+  final textController = TextEditingController();
+  String? photoURl;
+  String email = '';
+  File? imageFile;
+  bool isEditing = false;
+  bool _loading = false;
+
+
+
+
+
+  String extractNameFromEmail(String email) {
+    if (email.isEmpty) {
+      return '';
+    }
+    int index = email.indexOf('@');
+    if (index == -1) {
+      return email;
+    }
+    return email.substring(0, index);
+  }
+
+  // sign user out method
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+
+  Future<String> getUserPhotoUrl(String uid) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> userSnapshot =
+      await FirebaseFirestore.instance.collection('user').doc(uid).get();
+      if (userSnapshot.exists) {
+        Map<String, dynamic> userInfo = userSnapshot.data()!;
+        String photoUrl = userInfo['photoUrl'];
+        print(photoUrl);
+        return photoUrl;
+      } else {
+        return 'error not exist';
+      }
+    } catch (e) {
+      print('Error getting user info: $e');
+      return 'error get firebase';
+    }
+  }
+
+  void getUrl() async{
+    photoURl = await getUserPhotoUrl(user.email.toString().toLowerCase());
+    print('vghjohigufyghjkoihgutfguhijkojihgufydguhjnihugyfctgbhnkbjhgvyf');
+    print(photoURl);
+  }
+
+
+
+
+  @override
+  void initState() {
+    email = user.email.toString();
+    username = extractNameFromEmail(email);
+    setState(() {
+      username;
+    });
+
+    print(user);
+    getUrl();
+    setState(() {
+      print('22222222222222222222222');
+      print(photoURl);
+    });
+    super.initState();
+    controllers = videoLinks
+        .map((videoLink) => VideoPlayerController.network(videoLink))
+        .toList();
+    controllers.forEach((controller) {
+      controller.addListener(() {
+        if (controller.value.isInitialized) {
+          setState(() {});
+        }
+      });
+      controller.setLooping(true);
+      controller.initialize().then((_) => setState(() {}));
+      controller.pause();
+    });
+  }
+
+  @override
+  void dispose() {
+    controllers.forEach((controller) => controller.dispose());
+    super.dispose();
+  }
+
+  Future<void> _pickAndUploadImage() async {
+    // Ouvre la galerie de photos pour que l'utilisateur puisse sélectionner une photo
+    final pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      // Affiche un indicateur de progression
+      setState(() {
+        _loading = true;
+      });
+
+      // Crée une référence de stockage pour la photo
+      final fileName = basename(pickedFile.path);
+      final storageReference =
+      FirebaseStorage.instance.ref().child('images/$fileName');
+
+      // Charge la photo dans le stockage Firebase Storage
+      final uploadTask = storageReference.putFile(File(pickedFile.path));
+      final taskSnapshot = await uploadTask.whenComplete(() {});
+
+      // Récupère l'URL de téléchargement de la photo
+      final photoUrl = await taskSnapshot.ref.getDownloadURL();
+
+      // Met à jour le lien de la photo dans la base de données Firestore
+      final userId = FirebaseAuth.instance.currentUser?.uid;
+      final userRef = FirebaseFirestore.instance.collection('user').doc(userId);
+      await userRef.update({'photoUrl': photoUrl});
+
+      // Met à jour la vue avec la nouvelle photo
+      setState(() {
+        photoURl = photoUrl;
+        _loading = false;
+      });
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context, String nom, String prenom, List todo ) {
     return Scaffold(
